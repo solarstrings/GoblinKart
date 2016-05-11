6cfdb2abf790b01acc4e6ef75c50406d28a1306b
Ankan <andreas.m.karlsson@gmail.com>
2016-05-11 17:16:28 +0200

    A start on an observer pattern.

---
+++ GameEngine/Source/Observer/CollisionInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Observer
+{
+    public class CollisionInformation
+    {
+        public string Information { get; set; }
+    }
+}
