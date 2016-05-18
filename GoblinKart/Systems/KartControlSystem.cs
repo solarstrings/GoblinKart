c3a7bfa52c680fdcefa4e0eccc3fb79e5c620f2f
Axellent <axel.sigl@gmail.com>
2016-05-18 18:58:57 +0100

    Fixed gravity and jumping, added basic friction

--- GoblinKart/Systems/KartControlSystem.cs
+++ GoblinKart/Systems/KartControlSystem.cs
@@ -7,7 +7,15 @@ using Microsoft.Xna.Framework;
 namespace GoblinKart {
     class KartControlSystem : IUpdateSystem {
         ECSEngine engine;
-        bool jumping;
+        bool airBorne;
+        const float kartGroundOffset = 1.7f;
+        const float kartAcceleration = 2f;
+        const float kartTurningAcceleration = 2.8f;
+        const float kartFriction = 0.99f;
+        const float maxSpeed = 100f;
+        const float maxReverseSpeed = -50f;
+        const float gravityAcceleration = -100f;
+        const float jumpingAcceleration = 75f;
 
         public KartControlSystem(ECSEngine engine) {
             this.engine = engine;
@@ -22,24 +30,39 @@ namespace GoblinKart {
             Entity terrain = ComponentManager.Instance.GetEntityWithTag("Terrain", sceneEntities);
             TerrainMapComponent terComp = ComponentManager.Instance.GetEntityComponent<TerrainMapComponent>(terrain);
 
-            engine.SetWindowTitle("Visible Chunks:" +terComp.numChunksInView + " Num Drawed static models: "+terComp.numModelsInView + "| Kart x: " + trsComp.position.X + " Kart y: " + trsComp.position.Y + " Kart z: " + trsComp.position.Z + " Map height: " +
+            engine.SetWindowTitle("Visible Chunks:" + terComp.numChunksInView + " Num Drawed static models: " + terComp.numModelsInView + "| Kart x: " + trsComp.position.X + " Kart y: " + trsComp.position.Y + " Kart z: " + trsComp.position.Z + " Map height: " +
                 TerrainMapRenderSystem.GetTerrainHeight(terComp, trsComp.position.X, Math.Abs(trsComp.position.Z)));
 
             ModelRenderSystem.ResetMeshTransforms(ref kartModel);
             MoveKart(gameTime, sceneEntities, trsComp, kartModel);
+            ApplyGravity(trsComp, terComp, gameTime);
+            ApplyFriction(trsComp);
+        }
 
-            if (trsComp.position.Y <= TerrainMapRenderSystem.GetTerrainHeight(terComp, trsComp.position.X, Math.Abs(trsComp.position.Z)))
-            {
-                trsComp.LockModelToHeight(terComp);
+        /* Increases negative vertical acceleration of the kart if it is above the ground. If it only slightly above the ground
+        vertical acceleration is reduced to prevent passing through. */
+        public void ApplyGravity(TransformComponent trsComp, TerrainMapComponent terComp, GameTime gameTime) {
+            float distanceToGround = -(TerrainMapRenderSystem.GetTerrainHeight(terComp, trsComp.position.X, Math.Abs(trsComp.position.Z)) - trsComp.position.Y);
+
+            if (distanceToGround <= kartGroundOffset) {
+                trsComp.LockModelToHeight(terComp, kartGroundOffset);
                 trsComp.Velocity.Y = 0;
-                jumping = false;
+                airBorne = false;
             }
-            else
-            {
-                trsComp.Velocity.Y += -100f*(float)gameTime.ElapsedGameTime.TotalSeconds;
+            else {
+                if(distanceToGround > -gravityAcceleration * (float)gameTime.ElapsedGameTime.TotalSeconds) {
+                    trsComp.Velocity.Y += gravityAcceleration * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else {
+                    trsComp.Velocity.Y = distanceToGround;
+                }
                 trsComp.position += new Vector3(0, trsComp.Velocity.Y, 0) * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                jumping = true;
+                airBorne = true;
             }
+        }
+
+        public void ApplyFriction(TransformComponent trsComp) {
+            trsComp.Velocity.X *= kartFriction;
         }
 
         private void MoveKart(GameTime gameTime, List<Entity> sceneEntities, TransformComponent trsComp, ModelComponent kartModel) {
@@ -52,12 +75,12 @@ namespace GoblinKart {
 
                 if (k != null) {
                     if (Utilities.CheckKeyboardAction("right", BUTTON_STATE.HELD, k)) {
-                        newRot = new Vector3(-2.8f, 0f, 0f) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        newRot = new Vector3(-kartTurningAcceleration, 0f, 0f) * (float)gameTime.ElapsedGameTime.TotalSeconds;
                         trsComp.vRotation = newRot;
                         moving = true;
                     }
                     else if (Utilities.CheckKeyboardAction("left", BUTTON_STATE.HELD, k)) {
-                        newRot = new Vector3(2.8f, 0f, 0f) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        newRot = new Vector3(kartTurningAcceleration, 0f, 0f) * (float)gameTime.ElapsedGameTime.TotalSeconds;
                         trsComp.vRotation = newRot;
                         moving = true;
                     }
@@ -69,17 +92,19 @@ namespace GoblinKart {
                     }
 
                     if (Utilities.CheckKeyboardAction("forward", BUTTON_STATE.HELD, k)) {
-                        trsComp.Velocity += new Vector3(2f, 0, 0);
+                        if(trsComp.Velocity.X < maxSpeed) {
+                            trsComp.Velocity += new Vector3(kartAcceleration, 0, 0);
+                        }
                     }
                     if (Utilities.CheckKeyboardAction("back", BUTTON_STATE.HELD, k)) {
-                        trsComp.Velocity += new Vector3(-2f, 0, 0);
+                        if (trsComp.Velocity.X > maxReverseSpeed) {
+                            trsComp.Velocity += new Vector3(-kartAcceleration, 0, 0);
+                        }
                     }
-                    if (Utilities.CheckKeyboardAction("jump", BUTTON_STATE.RELEASED, k))
-                    {
-                        //if (jumping)
-                        //    return;
-                        trsComp.Velocity.Y += 50f;
-                        trsComp.position += new Vector3(0, trsComp.Velocity.Y, 0);
+                    if (Utilities.CheckKeyboardAction("jump", BUTTON_STATE.RELEASED, k)) {
+                        if (trsComp.Velocity.Y < maxSpeed) {
+                            trsComp.Velocity.Y += jumpingAcceleration;
+                        }
                     }
                     ModelRenderSystem.SetMeshTransform(ref kartModel, 1, Matrix.CreateRotationY(0.08f));
                     ModelRenderSystem.SetMeshTransform(ref kartModel, 3, Matrix.CreateRotationY(0.1f));
