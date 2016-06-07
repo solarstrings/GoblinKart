82ab58d18e763ca8a422a7c2573c2002cc3987ad
Oscar <Oscar@OSCAR-PC>
2016-06-07 02:24:57 +0200

    Split State

--- GameEngine/Systems/AiSystem.cs
+++ GameEngine/Systems/AiSystem.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
 using System.Threading.Tasks;
 using GameEngine.Components;
