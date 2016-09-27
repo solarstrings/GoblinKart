744df599f2479d891bb568d3b02aafd9e54db55d
Andreas Karlsson <andreas.m.karlsson@gmail.com>
2016-09-27 20:44:56 +0200

    CountLaps etc. Lite game logic typ

---
+++ GoblinKart/Systems/CheckIfPlayerWonSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngine.Components;
+using GameEngine.Interfaces;
+using GameEngine.Managers;
+using GoblinKart.Components;
+using Microsoft.Xna.Framework;
+
+namespace GoblinKart.Systems
+{
+    public class CheckIfPlayerWonSystem : IUpdateSystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            var playerEntities = ComponentManager.Instance.GetAllEntitiesWithComponentType<PlayerComponent>();
+            var gameSettings = ComponentManager.Instance.GetAllComponentsOfType<GameSettingsComponent>()[0];
+                
+            foreach (var e in playerEntities)
+            {
+                var playerLapComp = ComponentManager.Instance.GetEntityComponent<LapComponent>(e);
+                if (playerLapComp.Lap >= gameSettings.NrOfLaps)
+                {
+                    // A player has won
+                    // Handle it! Exit the game?
+                }
+            }
+        }
+    }
+}
